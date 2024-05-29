7473eb694b3c82de87db6f2bfc03f408216c5891
Rafael <tessalus3@gmail.com>
2024-05-29 13:12:52 -0300

    custom listener.

--- str-consumer/src/main/java/com/rafael/strconsumer/listener/StrConsumerListener.java
+++ str-consumer/src/main/java/com/rafael/strconsumer/listener/StrConsumerListener.java
@@ -2,8 +2,9 @@ package com.rafael.strconsumer.listener;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import com.rafael.strconsumer.custom.StrConsumerCustomListener;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -13,17 +14,17 @@ public class StrConsumerListener {
 
 	private static final Logger log = LogManager.getLogger(StrConsumerListener.class);
 	
-	@KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+	@StrConsumerCustomListener(groupId = "group-1")
 	public void create(String message) {
 		log.info("CREATE ::: message {}", message);
 	}
 	
-	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+	@StrConsumerCustomListener(groupId = "group-1")
 	public void log(String message) {
 		log.info("LOG ::: message {}", message);
 	}	
 	
-	@KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+	@StrConsumerCustomListener(groupId = "group-2")
 	public void history(String message) {
 		log.info("HISTORY ::: message {}", message);
 	}	
